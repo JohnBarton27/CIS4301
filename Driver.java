800b53e4005eb7c161bcdef6880fc9561b70ecb3
Kevin <kmarin@ufl.edu>
2015-12-06 16:58:54 -0500

    Revert "Framework for Orders"
    This reverts commit 3f7444150618073947a532dae5da631f225ba8e9.

--- Driver.java
+++ Driver.java
@@ -1,9 +1,6 @@
 import java.io.* ;
 import java.sql.* ;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.Date;
 import java.util.regex.Pattern;
 
 /*
@@ -36,21 +33,39 @@ public class Driver
     static Statement sqlStatement  = null;
     static ResultSet myResultSet  = null;
     
-    final static int SHOP_USER_IDX = 0;
-    final static int ORDERS_IDX = 1;
-    final static int ORDER1_IDX = 2;
-    final static int CONTAINS_IDX = 3;
-    final static int PRODUCT_IDX = 4;
-    final static int LOCATION_IDX = 5;
-    final static int SHELF_IDX = 6;
-    final static int SUPPLYS_IDX = 7;
-    final static int SUPPLIER_IDX = 8;
-    final static int INSIDE_IDX = 9;
-    final static int PARENT_IDX = 10;
-    final static int CATEGORY_IDX = 11;
-    final static int AVAILABLE_IDX = 12;
+    //Indexes
+    final static int SHOP_USER_IDX    = 0;
+    final static int ORDERS_IDX       = 1;
+    final static int ORDER1_IDX       = 2;
+    final static int CONTAINS_IDX     = 3;
+    final static int PRODUCT_IDX      = 4;
+    final static int LOCATION_IDX     = 5;
+    final static int SHELF_IDX        = 6;
+    final static int SUPPLYS_IDX      = 7;
+    final static int SUPPLIER_IDX     = 8;
+    final static int INSIDE_IDX       = 9;
+    final static int PARENT_IDX       = 10;
+    final static int CATEGORY_IDX     = 11;
+    final static int AVAILABLE_IDX    = 12;
     final static int AVAILABLECAT_IDX = 13;
-    final static int DISCOUNT_IDX = 14;
+    final static int DISCOUNT_IDX     = 14;
+    
+    //Initial IDs
+    final static int SHOP_USER_INIT    = 100000;
+    final static int ORDERS_INIT       = 150000;
+    final static int ORDER1_INIT       = 200000;
+    final static int CONTAINS_INIT     = 250000;
+    final static int PRODUCT_INIT      = 300000;
+    final static int LOCATION_INIT     = 350000;
+    final static int SHELF_INIT        = 400000;
+    final static int SUPPLYS_INIT      = 450000;
+    final static int SUPPLIER_INIT     = 500000;
+    final static int INSIDE_INIT       = 530000;
+    final static int PARENT_INIT       = 560000;
+    final static int CATEGORY_INIT     = 600000;
+    final static int AVAILABLE_INIT    = 630000;
+    final static int AVAILABLECAT_INIT = 660000;
+    final static int DISCOUNT_INIT     = 700000;
     
 	static int[] lastIds = new int[15];
 	
@@ -252,7 +267,7 @@ public class Driver
 		System.out.println("Select a number:"
 				+ "\n1 Update account info"
 				+ "\n2 Delete account"
-				+ "\n3 Modify an order"
+				+ "\n3 Add product to order"
 				+ "\n4 Checkout"
 				+ "\n5 Logout");
 		
@@ -266,7 +281,7 @@ public class Driver
 				deleteCurrUser();
 				break;
 			case 3:
-				modifyOrders(false);
+				addProductToOrder();
 				break;
 //			case 4:
 //				checkout();
@@ -277,7 +292,7 @@ public class Driver
 		}
 	}
 	
-	public static void loggedInStaff() {
+	private static void loggedInStaff() {
 		System.out.println("Select a number:"
 				+ "\n1 Update account info"
 				+ "\n2 Delete account"
@@ -286,7 +301,8 @@ public class Driver
 				+ "\n5 Orders"
 				+ "\n6 Categories"
 				+ "\n7 Discounts"
-				+ "\n8 Logout");
+				+ "\n8 More Choices"
+				+ "\n9 Logout");
 		
 		int op = sc.nextInt();
 		
@@ -303,18 +319,43 @@ public class Driver
 			case 4: 
 				modifyProducts();
 				break;
-			case 5:
-				modifyOrders(true);
-				break;
-//			case 6:
-//				modifyCategories();
+//			case 5:
+//				modifyOrders();
 //				break;
+			case 6:
+				modifyCategories();
+				break;
 //			case 7:
 //				modifyDiscounts();
 			case 8:
+				staffMore();
+				break;
+			case 9:
 				logoutUser();
 				break;
 		}
+	}
+	
+	private static void staffMore() {
+		System.out.println("Select a number:"
+				+ "\n1 Back"
+				+ "\n2 Shelves"
+				+ "\n3 Suppliers");
+		
+		int op = sc.nextInt();
+		
+		switch (op) {
+			case 1:
+				loggedInStaff();
+				break;
+			case 2:
+				modifyShelves();
+			case 3:
+				modifySuppliers();
+				break;
+		}
+		
+		loggedInStaff();
 	}
 	
 	/////////
@@ -359,7 +400,7 @@ public class Driver
 		
 		int id;
 		if (lastIds[SHOP_USER_IDX] == 0) {
-			id = 100000;
+			id = SHOP_USER_INIT;
 		} else {
 			id = lastIds[SHOP_USER_IDX] + 1;
 		}
@@ -503,7 +544,7 @@ public class Driver
 		}
 	}
 	
-	/**public static void addProductToOrder() {
+	public static void addProductToOrder() {
 		System.out.println("Which product would you like to add to your order?"
 				+ "\n1 Product1"
 				+ "\n2 Product2"
@@ -511,82 +552,7 @@ public class Driver
 		int prodSel = sc.nextInt();
 		
 		//TODO NEEDS MORE
-	}**/
-	
-	private static void modifyOrders(boolean isStaff) {
-		System.out.println("Add [A], Update [U], or Delete [D] an order?");
-		String sel = sc.next();
-		
-		if (sel.equals("A")) {
-			addNewOrder();
-			loggedInCust();
-		} else if (sel.equals("U")) {
-			updateOrder();
-			loggedInCust();
-		}
-		  else if(sel.equals("D")){
-			if(isStaff == false)
-				System.out.println("Only staff may modify orders");
-				else 
-					deleteOrder(); 
-		}
-			
 	}
-	
-	private static void addNewOrder() {
-		
-		boolean paid = false;
-		DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-		Date date = new Date();
-		String currDate = dateFormat.format(date);
-		
-		int id;
-		if (lastIds[ORDER1_IDX] == 0) {
-			id = 100000;
-		} else {
-			id = lastIds[ORDER1_IDX] + 1;
-		}
-		lastIds[ORDER1_IDX] = id;
-		
-		String q = "INSERT INTO ORDER1 VALUES (" + id +", " + 0 +", '" + currDate + "', '" + paid + "')";
-		
-		executeQuery(q);
-		
-		loggedInStaff();
-	}
-	
-	private static void updateOrder() {
-		System.out.println("Which order do you want to update? (ID)");
-		printAllOrders();
-		
-		int idToUpdate = sc.nextInt();
-		//TODO Need to link this to products and figure out how to add and delete
-		// Need to be able to add AND delete products
-		loggedInStaff();
-	}
-	
-		//TODO figure out a query to just return the orders of the current user as well as return products in the order
-	
-	private static void printAllOrders() {
-		String q = "SELECT id, total_price, date, paid FROM ORDER1 ORDER by id";
-		ResultSet allOrders = executeQuery(q);
-		
-		try {
-			while(allOrders.next())
-			{
-				int id = allOrders.getInt(1);
-				int price = allOrders.getInt(2);
-				String date = allOrders.getString(3);
-				String paid = allOrders.getString(4);
-				System.out.println(id + ": " + price + " - " + date + " - " + paid);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return;
-	}
-	
 	
 	public static void logoutUser() {
 		loggedInUser = 0;
@@ -608,7 +574,7 @@ public class Driver
 		
 		int id;
 		if (lastIds[SHOP_USER_IDX] == 0) {
-			id = 100000;
+			id = SHOP_USER_INIT;
 		} else {
 			id = lastIds[SHOP_USER_IDX] + 1;
 		}
@@ -626,21 +592,11 @@ public class Driver
 		}
 	}
 	
-	private static void deleteOrder() {
-		System.out.println("Which order do you want to delete? (ID)");
-		printAllUsers();
-		
-		int idToDelete = sc.nextInt();
-		String q = "DELETE FROM ORDER1 WHERE id = " + idToDelete;
-		executeQuery(q);
-		loggedInStaff();
-	}
-	
 	////////////
 	//PRODUCTS//
 	////////////
 	private static void modifyProducts() {
-		System.out.println("Add [A], Update [U], or Delete [D] a User?");
+		System.out.println("Add [A], Update [U], or Delete [D] a Product?");
 		String sel = sc.next();
 		
 		if (sel.equals("A")) {
@@ -655,15 +611,420 @@ public class Driver
 	}
 	
 	private static void addNewProduct() {
+		System.out.print("Product name: ");
+		String name = sc.next();
 		
+		System.out.print("Product price: ");
+		String price = sc.next();
+		
+		System.out.print("Product stock quantity: ");
+		int stockQuantity = sc.nextInt();
+		
+		System.out.print("Product description: ");
+		String description = sc.next();
+		
+		System.out.println("Product location (ID): ");
+		printAllShelfs();
+		int shelfId = sc.nextInt();
+		
+		System.out.println("Product supplier (ID): ");
+		printAllSuppliers();
+		int supplierId = Integer.parseInt(sc.next());
+		
+		System.out.println("Product Category (ID): ");
+		printAllCategories();
+		int categoryId = Integer.parseInt(sc.next());
+		
+		int id;
+		if (lastIds[PRODUCT_IDX] == 0) {
+			id = PRODUCT_INIT;
+		} else {
+			id = lastIds[PRODUCT_IDX] + 1;
+		}
+		lastIds[PRODUCT_IDX] = id;
+		
+		int locationId;
+		if (lastIds[LOCATION_IDX] == 0) {
+			locationId = LOCATION_INIT;
+		} else {
+			locationId = lastIds[LOCATION_IDX] + 1;
+		}
+		lastIds[LOCATION_IDX] = locationId;
+		
+		String q = "INSERT INTO LOCATION VALUES (" + locationId + ", "  + ", " + id + ", " + shelfId + ")";
+		executeQuery(q);
+		
+		int supplysId;
+		if (lastIds[SUPPLYS_IDX] == 0) {
+			supplysId = SUPPLYS_INIT;
+		} else {
+			supplysId = lastIds[SUPPLYS_IDX] + 1;
+		}
+		lastIds[SUPPLYS_IDX] = supplysId;
+		
+		q = "INSERT INTO SUPPLYS VALUES (" + supplysId + ", " + supplierId + ")";
+		executeQuery(q);
+		
+		int inId;
+		if (lastIds[INSIDE_IDX] == 0) {
+			inId = INSIDE_INIT;
+		} else {
+			inId = lastIds[INSIDE_IDX] + 1;
+		}
+		lastIds[INSIDE_IDX] = inId;
+
+		q = "INSERT INTO INSIDE VALUES (" + inId + ", " + categoryId + ")";
+		executeQuery(q);
+
+		
+		q = "INSERT INTO PRODUCT VALUES (" + id + ", '" + name + "', " + price + ", " + stockQuantity + ", '" + description + "', true, '0', " + inId + ", " + supplysId;
+		executeQuery(q);
+		loggedInStaff();
 	}
 	
 	private static void updateProduct() {
 		
+		loggedInStaff();
 	}
 	
 	private static void deleteProduct() {
 		
+		loggedInStaff();
+	}
+	
+	private static void printAllProducts() {
+		String q = "SELECT id, name, price, description, active FROM PRODUCT ORDER by id";
+		ResultSet allProducts = executeQuery(q);
+		
+		try {
+			while(allProducts.next())
+			{
+				int id = allProducts.getInt(1);
+				String name = allProducts.getString(2);
+				String price = allProducts.getString(3);
+				String description = allProducts.getString(4);
+				String active = allProducts.getString(5);
+				if (active.equals("true")) {
+					active = "Active";
+				} else {
+					active = "Not Active";
+				}
+				System.out.println(id + ": " + name + " - " + description + " - " + price + " - " + active);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return;
+	}
+	
+	//////////////
+	//CATEGORIES//
+	//////////////
+	private static void modifyCategories() {
+		System.out.println("Add [A], Update [U], or Delete [D] a Category?");
+		String sel = sc.next();
+		
+		if (sel.equals("A")) {
+			addNewCategory();
+		} else if (sel.equals("U")) {
+			updateCategory();
+		} else if (sel.equals("D")) {
+			deleteCategory();
+		}
+		
+		loggedInStaff();
+	}
+	
+	private static void addNewCategory() {
+		System.out.print("Category name: ");
+		String name = sc.next();
+		
+		System.out.print("Category description: ");
+		String description = sc.next();
+		
+		System.out.println("Parent Category? (ID, 'N' for none)");
+		printAllCategories();
+		String parCar = sc.next();
+		
+		int id;
+		if (lastIds[CATEGORY_IDX] == 0) {
+			id = CATEGORY_INIT;
+		} else {
+			id = lastIds[CATEGORY_IDX] + 1;
+		}
+		lastIds[CATEGORY_IDX] = id;
+		
+		if (!parCar.equals("N")) {
+			int parCarId = Integer.parseInt(parCar);
+			
+			int parentId;
+			if (lastIds[PARENT_IDX] == 0) {
+				parentId = PARENT_INIT;
+			} else {
+				parentId = lastIds[PARENT_IDX] + 1;
+			}
+			lastIds[PARENT_IDX] = parentId;
+			
+			String q = "INSERT INTO PARENT VALUES (" + parentId + ", " + id + ", " + parCarId + ")";
+			executeQuery(q);
+		}		
+		
+		String q = "INSERT INTO CATEGORY VALUES (" + id +", '" + name +"', '" + description + "')";
+		executeQuery(q);
+		
+		loggedInStaff();
+	}
+	
+	private static void updateCategory() {
+		System.out.println("Which category do you want to update? (ID)");
+		printAllCategories();
+		
+		int idToUpdate = sc.nextInt();
+		//Name
+		System.out.print("Update name? [Y] or [N]");
+		if (sc.next().equals("Y")) {
+			System.out.print("New name: ");
+			String q = "UPDATE CATEGORY SET name = '" + sc.next() + "' WHERE id = " + idToUpdate;
+			executeQuery(q);
+		}
+		
+		//Description
+		System.out.print("Update description? [Y] or [N]");
+		if (sc.next().equals("Y")) {
+			System.out.print("New description: ");
+			String q = "UPDATE CATEGORY SET description = '" + sc.next() + "' WHERE id = " + idToUpdate;
+			executeQuery(q);
+		}
+		
+		//Parent category
+		System.out.println("Update Parent Category? [Y] or [N]");
+		if (sc.next().equals("Y")) {
+			System.out.println("Which category? (ID, 'N' for none)");
+			printAllCategories();
+			String parCar = sc.next();
+			
+			if (!parCar.equals("N")) {
+				int parCarId = Integer.parseInt(parCar);
+				
+				int parentId;
+				if (lastIds[PARENT_IDX] == 0) {
+					parentId = PARENT_INIT;
+				} else {
+					parentId = lastIds[PARENT_IDX] + 1;
+				}
+				lastIds[PARENT_IDX] = parentId;
+				
+				String q = "INSERT INTO PARENT VALUES (" + parentId + ", " + idToUpdate + ", " + parCarId + ")";
+				executeQuery(q);
+			} else {
+				String q = "DELETE FROM PARENT WHERE child_category_id = " + idToUpdate;
+				executeQuery(q);
+			}
+		}
+		
+		loggedInStaff();		
+	}
+	
+	private static void deleteCategory() {
+		System.out.println("Which category do you want to update? (ID)");
+		printAllCategories();
+		
+		int idToDelete = sc.nextInt();
+		String q = "DELETE FROM CATEGORY WHERE id = " + idToDelete;
+		executeQuery(q);
+		loggedInStaff();
+	}
+	
+	private static void printAllCategories() {
+		String q = "SELECT id, name, description FROM CATEGORY ORDER by id";
+		ResultSet allCategories = executeQuery(q);
+		
+		try {
+			while(allCategories.next())
+			{
+				int id = allCategories.getInt(1);
+				String name = allCategories.getString(2);
+				String description = allCategories.getString(3);
+				System.out.println(id + ": " + name + " - " + description);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return;
+	}
+	
+	///////////
+	//SHELVES//
+	///////////
+	private static void modifyShelves() {
+		System.out.println("Add [A], Update [U], or Delete [D] a Shelf?");
+		String sel = sc.next();
+		
+		if (sel.equals("A")) {
+			addNewShelf();
+		} else if (sel.equals("U")) {
+			updateShelf();
+		} else if (sel.equals("D")) {
+			deleteShelf();
+		}
+		
+		loggedInStaff();
+	}
+	
+	private static void addNewShelf() {
+		System.out.print("Shelf name: ");
+		String name = sc.next();
+		
+		System.out.print("Available Quantity on Shelf: ");
+		int quantity = sc.nextInt();
+		
+		int id;
+		if (lastIds[SHELF_IDX] == 0) {
+			id = SHELF_INIT;
+		} else {
+			id = lastIds[SHELF_IDX] + 1;
+		}
+		lastIds[SHELF_IDX] = id;
+		
+		String q = "INSERT INTO SHELF (id, name, available_quantity) VALUES (" + id +", '" + name +"', " + quantity + ")";
+		executeQuery(q);
+		
+		loggedInStaff();
+	}
+	
+	private static void updateShelf() {
+		System.out.println("Which shelf do you want to update? (ID)");
+		printAllShelfs();
+		
+		int idToUpdate = sc.nextInt();
+		//Name
+		System.out.print("Update name? [Y] or [N]");
+		if (sc.next().equals("Y")) {
+			System.out.print("New name: ");
+			String q = "UPDATE SHELF SET name = '" + sc.next() + "' WHERE id = " + idToUpdate;
+			executeQuery(q);
+		}
+		
+		//Quantity
+		System.out.print("Update available quantity? [Y] or [N]");
+		if (sc.next().equals("Y")) {
+			System.out.print("New available quanity: ");
+			String q = "UPDATE SHELF SET available_quantity = '" + sc.next() + "' WHERE id = " + idToUpdate;
+			executeQuery(q);
+		}
+		
+		loggedInStaff();		
+	}
+	
+	private static void deleteShelf() {
+		System.out.println("Which shelf do you want to update? (ID)");
+		printAllShelfs();
+		
+		int idToDelete = sc.nextInt();
+		String q = "DELETE FROM SHELF WHERE id = " + idToDelete;
+		executeQuery(q);
+		loggedInStaff();
+	}
+	
+	private static void printAllShelfs() {
+		String q = "SELECT id, name, available_quantity FROM SHELF ORDER by id";
+		ResultSet allShelfs = executeQuery(q);
+		
+		try {
+			while(allShelfs.next())
+			{
+				int id = allShelfs.getInt(1);
+				String name = allShelfs.getString(2);
+				int quantity = allShelfs.getInt(3);
+				System.out.println(id + ": " + name + " - " + quantity);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return;
+	}
+	
+	/////////////
+	//SUPPLIERS//
+	/////////////
+	private static void modifySuppliers() {
+		System.out.println("Add [A], Update [U], or Delete [D] a Supplier?");
+		String sel = sc.next();
+		
+		if (sel.equals("A")) {
+			addNewSupplier();
+		} else if (sel.equals("U")) {
+			updateSupplier();
+		} else if (sel.equals("D")) {
+			deleteSupplier();
+		}
+		
+		loggedInStaff();
+	}
+	
+	private static void addNewSupplier() {
+		System.out.print("Supplier name: ");
+		String name = sc.next();
+				
+		int id;
+		if (lastIds[SUPPLIER_IDX] == 0) {
+			id = SUPPLIER_INIT;
+		} else {
+			id = lastIds[SUPPLIER_IDX] + 1;
+		}
+		lastIds[SUPPLIER_IDX] = id;
+		
+		String q = "INSERT INTO SUPPLIER VALUES (" + id +", '" + name + "')";
+		executeQuery(q);
+		
+		loggedInStaff();
+	}
+	
+	private static void updateSupplier() {
+		System.out.println("Which supplier do you want to update? (ID)");
+		printAllSuppliers();
+		
+		int idToUpdate = sc.nextInt();
+		//Name
+		System.out.print("Update name? [Y] or [N]");
+		if (sc.next().equals("Y")) {
+			System.out.print("New name: ");
+			String q = "UPDATE SUPPLIER SET name = '" + sc.next() + "' WHERE id = " + idToUpdate;
+			executeQuery(q);
+		}
+				
+		loggedInStaff();		
+	}
+	
+	private static void deleteSupplier() {
+		System.out.println("Which supplier do you want to update? (ID)");
+		printAllSuppliers();
+		
+		int idToDelete = sc.nextInt();
+		String q = "DELETE FROM SUPPLIER WHERE id = " + idToDelete;
+		executeQuery(q);
+		loggedInStaff();
+	}
+	
+	private static void printAllSuppliers() {
+		String q = "SELECT id, name, FROM SUPPLIER ORDER by id";
+		ResultSet allSuppliers = executeQuery(q);
+		
+		try {
+			while(allSuppliers.next())
+			{
+				int id = allSuppliers.getInt(1);
+				String name = allSuppliers.getString(2);
+				System.out.println(id + ": " + name);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return;
 	}
 	
 }
