cefaff22d1d0e52c43167d67081021e73d16be6a
JohnBarton27 <johnrbarton@ufl.edu>
2015-12-06 14:10:06 -0500

    Products ALMOST working, getting SQL missing expression error

--- Driver.java
+++ Driver.java
@@ -322,9 +322,9 @@ public class Driver
 //			case 5:
 //				modifyOrders();
 //				break;
-//			case 6:
-//				modifyCategories();
-//				break;
+			case 6:
+				modifyCategories();
+				break;
 //			case 7:
 //				modifyDiscounts();
 			case 8:
@@ -617,6 +617,9 @@ public class Driver
 		System.out.print("Product price: ");
 		String price = sc.next();
 		
+		System.out.print("Product stock quantity: ");
+		int stockQuantity = sc.nextInt();
+		
 		System.out.print("Product description: ");
 		String description = sc.next();
 		
@@ -624,8 +627,58 @@ public class Driver
 		printAllShelfs();
 		int shelfId = sc.nextInt();
 		
+		System.out.println("Product supplier (ID): ");
+		printAllSuppliers();
+		int supplierId = Integer.parseInt(sc.next());
 		
+		System.out.println("Product Category (ID): ");
+		printAllCategories();
+		int categoryId = Integer.parseInt(sc.next());
 		
+		int id;
+		if (lastIds[PRODUCT_IDX] == 0) {
+			id = PRODUCT_INIT;
+		} else {
+			id = lastIds[PRODUCT_IDX] + 1;
+		}
+		lastIds[PRODUCT_IDX] = id;
+		
+		int locationId;
+		if (lastIds[LOCATION_IDX] == 0) {
+			locationId = LOCATION_INIT;
+		} else {
+			locationId = lastIds[LOCATION_IDX] + 1;
+		}
+		lastIds[LOCATION_IDX] = locationId;
+		
+		String q = "INSERT INTO LOCATION VALUES (" + locationId + ", "  + ", " + id + ", " + shelfId + ")";
+		executeQuery(q);
+		
+		int supplysId;
+		if (lastIds[SUPPLYS_IDX] == 0) {
+			supplysId = SUPPLYS_INIT;
+		} else {
+			supplysId = lastIds[SUPPLYS_IDX] + 1;
+		}
+		lastIds[SUPPLYS_IDX] = supplysId;
+		
+		q = "INSERT INTO SUPPLYS VALUES (" + supplysId + ", " + supplierId + ")";
+		executeQuery(q);
+		
+		int inId;
+		if (lastIds[INSIDE_IDX] == 0) {
+			inId = INSIDE_INIT;
+		} else {
+			inId = lastIds[INSIDE_IDX] + 1;
+		}
+		lastIds[INSIDE_IDX] = inId;
+
+		q = "INSERT INTO INSIDE VALUES (" + inId + ", " + categoryId + ")";
+		executeQuery(q);
+
+		
+		q = "INSERT INTO PRODUCT VALUES (" + id + ", '" + name + "', " + price + ", " + stockQuantity + ", '" + description + "', true, '0', " + inId + ", " + supplysId;
+		executeQuery(q);
 		loggedInStaff();
 	}
 	
@@ -657,6 +710,143 @@ public class Driver
 					active = "Not Active";
 				}
 				System.out.println(id + ": " + name + " - " + description + " - " + price + " - " + active);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return;
+	}
+	
+	//////////////
+	//CATEGORIES//
+	//////////////
+	private static void modifyCategories() {
+		System.out.println("Add [A], Update [U], or Delete [D] a Category?");
+		String sel = sc.next();
+		
+		if (sel.equals("A")) {
+			addNewCategory();
+		} else if (sel.equals("U")) {
+			updateCategory();
+		} else if (sel.equals("D")) {
+			deleteCategory();
+		}
+		
+		loggedInStaff();
+	}
+	
+	private static void addNewCategory() {
+		System.out.print("Category name: ");
+		String name = sc.next();
+		
+		System.out.print("Category description: ");
+		String description = sc.next();
+		
+		System.out.println("Parent Category? (ID, 'N' for none)");
+		printAllCategories();
+		String parCar = sc.next();
+		
+		int id;
+		if (lastIds[CATEGORY_IDX] == 0) {
+			id = CATEGORY_INIT;
+		} else {
+			id = lastIds[CATEGORY_IDX] + 1;
+		}
+		lastIds[CATEGORY_IDX] = id;
+		
+		if (!parCar.equals("N")) {
+			int parCarId = Integer.parseInt(parCar);
+			
+			int parentId;
+			if (lastIds[PARENT_IDX] == 0) {
+				parentId = PARENT_INIT;
+			} else {
+				parentId = lastIds[PARENT_IDX] + 1;
+			}
+			lastIds[PARENT_IDX] = parentId;
+			
+			String q = "INSERT INTO PARENT VALUES (" + parentId + ", " + id + ", " + parCarId + ")";
+			executeQuery(q);
+		}		
+		
+		String q = "INSERT INTO CATEGORY VALUES (" + id +", '" + name +"', '" + description + "')";
+		executeQuery(q);
+		
+		loggedInStaff();
+	}
+	
+	private static void updateCategory() {
+		System.out.println("Which category do you want to update? (ID)");
+		printAllCategories();
+		
+		int idToUpdate = sc.nextInt();
+		//Name
+		System.out.print("Update name? [Y] or [N]");
+		if (sc.next().equals("Y")) {
+			System.out.print("New name: ");
+			String q = "UPDATE CATEGORY SET name = '" + sc.next() + "' WHERE id = " + idToUpdate;
+			executeQuery(q);
+		}
+		
+		//Description
+		System.out.print("Update description? [Y] or [N]");
+		if (sc.next().equals("Y")) {
+			System.out.print("New description: ");
+			String q = "UPDATE CATEGORY SET description = '" + sc.next() + "' WHERE id = " + idToUpdate;
+			executeQuery(q);
+		}
+		
+		//Parent category
+		System.out.println("Update Parent Category? [Y] or [N]");
+		if (sc.next().equals("Y")) {
+			System.out.println("Which category? (ID, 'N' for none)");
+			printAllCategories();
+			String parCar = sc.next();
+			
+			if (!parCar.equals("N")) {
+				int parCarId = Integer.parseInt(parCar);
+				
+				int parentId;
+				if (lastIds[PARENT_IDX] == 0) {
+					parentId = PARENT_INIT;
+				} else {
+					parentId = lastIds[PARENT_IDX] + 1;
+				}
+				lastIds[PARENT_IDX] = parentId;
+				
+				String q = "INSERT INTO PARENT VALUES (" + parentId + ", " + idToUpdate + ", " + parCarId + ")";
+				executeQuery(q);
+			} else {
+				String q = "DELETE FROM PARENT WHERE child_category_id = " + idToUpdate;
+				executeQuery(q);
+			}
+		}
+		
+		loggedInStaff();		
+	}
+	
+	private static void deleteCategory() {
+		System.out.println("Which category do you want to update? (ID)");
+		printAllCategories();
+		
+		int idToDelete = sc.nextInt();
+		String q = "DELETE FROM CATEGORY WHERE id = " + idToDelete;
+		executeQuery(q);
+		loggedInStaff();
+	}
+	
+	private static void printAllCategories() {
+		String q = "SELECT id, name, description FROM CATEGORY ORDER by id";
+		ResultSet allCategories = executeQuery(q);
+		
+		try {
+			while(allCategories.next())
+			{
+				int id = allCategories.getInt(1);
+				String name = allCategories.getString(2);
+				String description = allCategories.getString(3);
+				System.out.println(id + ": " + name + " - " + description);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
